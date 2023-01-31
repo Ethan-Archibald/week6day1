f5a15b25b650259d85acfc03765b49bffabef406
Ethan.Archibald <ethan.archibald0426@stu.mtec.edu>
2023-01-31 10:10:56 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+let k = storeItems.forEach(function(item){
+    if (item.inStock == true){
+        $('#content').append(`<div class="inStock" ><p id="stockPrice">$${item.price}</p> <p id="stockName">${item.name}</p> <p id="stockDetails">${item.details}</p></div>`);
+    } else if (item.inStock == false){
+        $('#content').append('<p class="notInStock" >Out of Stock</p>');
+    }
+});
+
+const button = document.getElementById('clickMe');
+
+button.addEventListener('click', function() {
+  $('#mainConainer').toggleClass('darkMode');
+});
+
